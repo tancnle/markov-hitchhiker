44e59eeca42a6098b04069f404a88a61354b2bf1
Tan Le <tanle.oz@gmail.com>
2015-04-14 22:26:34 +1000

    Text mashup for Hitchhiker's Guide using Markov chain

--- src/markov_hitchhiker/generator.clj
+++ src/markov_hitchhiker/generator.clj
@@ -1,1 +1,66 @@
 (ns markov-hitchhiker.generator)
+
+(defn word-chain [word-transitions]
+  (reduce (fn [r t] (merge-with clojure.set/union r
+                               (let [[a b c] t]
+                                 {[a b] (if c #{c} #{})})))
+          {}
+          word-transitions))
+
+(defn text->word-chain [s]
+  (let [words (clojure.string/split s #"[\s|\n]")
+        word-transitions (partition-all 3 1 words)]
+    (word-chain word-transitions)))
+
+(defn chain->text [chain]
+  (apply str (interpose " " chain)))
+
+(defn walk-chain [prefix chain result]
+  (let [suffixes (get chain prefix)]
+    (if (empty? suffixes)
+      result
+      (let [suffix (first (shuffle suffixes))
+            new-prefix [(last prefix) suffix]
+            result-with-spaces (chain->text result)
+            result-char-count (count result-with-spaces)
+            suffix-char-count (inc (count suffix))
+            new-result-char-count (+ result-char-count suffix-char-count)]
+        (if (>= new-result-char-count 140)
+          result
+          (recur new-prefix chain (conj result suffix)))))))
+
+(defn generate-text [start-phrase word-chain]
+  (let [prefix (clojure.string/split start-phrase #" ")
+        result-chain (walk-chain prefix word-chain prefix)
+        result-text (chain->text result-chain)]
+    result-text))
+
+(defn process-file [fname]
+  (text->word-chain
+   (slurp (clojure.java.io/resource fname))))
+
+(def files ["hitchhiker.txt" "react.txt" "golden-bird.txt" "ecmascript.txt" "sherlock-holmes.txt" "webpack.txt"])
+
+(def functional-hitchhiker (apply merge-with clojure.set/union (map process-file files)))
+
+(def prefix-list ["Far out" "western spiral" "which was" "You can"
+                  "million miles" "interstellar hitchhiker" "Now it"
+                  "in emergencies"])
+
+(defn end-at-last-punctuation [text]
+  (let [trimmed-to-last-punct (apply str (re-seq #"[\s\w]+[^.!?,]*[.!?,]" text))
+        trimmed-to-last-word (apply str (re-seq #".*[^a-zA-Z]+" text))
+        result-text (if (empty? trimmed-to-last-punct)
+                      trimmed-to-last-word
+                      trimmed-to-last-punct)
+        cleaned-text (clojure.string/replace result-text #"[,| ]$" ".")]
+    (clojure.string/replace cleaned-text #"[\"|\(|\)|\]|\[]" "'")))
+
+(defn tweet-text []
+  (let [text (generate-text (-> prefix-list shuffle first) functional-hitchhiker)]
+    (end-at-last-punctuation text)))
+
+(defn -main [& args]
+  (println "Started up")
+  (doseq [x [1 2 3 4 5 6]]
+    (println (tweet-text))))
--- test/markov_hitchhiker/generator_test.clj
+++ test/markov_hitchhiker/generator_test.clj
@@ -2,6 +2,81 @@
   (:require [clojure.test :refer :all]
             [markov-hitchhiker.generator :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-word-chain
+  (testing "it produces a chain of the possible two step transitions between suffixes and prefixes"
+    (let [example '(("A" "certain" "king")
+                    ("certain" "king" "had")
+                    ("king" "had" "a")
+                    ("beautiful" "garden" "and"))]
+      (is (= {["A" "certain"] #{"king"}
+              ["beautiful" "garden"] #{"and"}
+              ["certain" "king"] #{"had"}
+              ["king" "had"] #{"a"}}
+             (word-chain example))))))
+
+(deftest test-text->word-chain
+  (testing "string with spaces and newlines"
+    (let [example "A certain king had a beautiful garden and"]
+     (is (= {["A" "certain"] #{"king"}
+             ["a" "beautiful"] #{"garden"}
+             ["and" nil] #{}
+             ["beautiful" "garden"] #{"and"}
+             ["certain" "king"] #{"had"}
+             ["garden" "and"] #{}
+             ["had" "a"] #{"beautiful"}
+             ["king" "had"] #{"a"}}
+            (text->word-chain example))))))
+
+(deftest test-walk-chain
+  (let [chain {["A" "certain"] #{"king"}
+               ["and" nil] #{}
+               ["beautiful" "garden"] #{"and"}
+               ["certain" "king"] #{"had"}
+               ["garden" "and"] #{}
+               ["had" "a"] #{"beautiful"}
+               ["king" "had"] #{"a"}
+               ["a" "beautiful"] #{"garden" "king"}
+               ["beautiful" "king"] #{"had"}}]
+    (testing "dead end"
+      (let [prefix ["beautiful" "garden"]]
+        (is (= ["beautiful" "garden" "and"]
+               (walk-chain prefix chain prefix)))))
+    (testing "multiple choices"
+      (with-redefs [shuffle (fn [c] c)]
+        (let [prefix ["a" "beautiful"]]
+          (is (= ["a" "beautiful" "garden" "and"]
+                 (walk-chain prefix chain prefix))))))
+      (with-redefs [shuffle (fn [c] (reverse c))]
+        (let [prefix ["king" "had"]]
+          (is (> 140
+                 (count (apply str (walk-chain prefix chain prefix)))))
+          (is (= ["king" "had" "a" "beautiful" "king" "had" "a" "beautiful"]
+                 (take 8 (walk-chain prefix chain prefix))))))))
+
+(deftest test-generate-text
+  (with-redefs [shuffle (fn [c] c)]
+    (let [chain {["A" "certain"] #{"king"}
+                 ["and" nil] #{}
+                 ["beautiful" "garden"] #{"and"}
+                 ["certain" "king"] #{"had"}
+                 ["garden" "and"] #{}
+                 ["had" "a"] #{"beautiful"}
+                 ["king" "had"] #{"a"}
+                 ["a" "beautiful"] #{"garden" "king"}
+                 ["beautiful" "king"] #{"had"}}]
+      (is (= "beautiful garden and" (generate-text "beautiful garden" chain)))
+      (is (= "beautiful king had a beautiful garden and" (generate-text "beautiful king" chain)))))) 
+
+(deftest test-end-at-last-punctuation
+  (testing "Ends at the last puncuation"
+    (is (= "In a tree so happy are we."
+           (end-at-last-punctuation "In a tree so happy are we. So that")))
+    (testing "Replaces ending comma with a period"
+    (is (= "In a tree so happy are we."
+           (end-at-last-punctuation "In a tree so happy are we, So that"))))
+    (testing "If there are no previous puncations, just leave it alone and add one at the end"
+      (is ( = "In the light of the blue moon."
+              (end-at-last-punctuation  "In the light of the blue moon there"))))
+    (testing "works with multiple punctuation"
+      (is ( = "In the light of the blue moon.  We danced merrily."
+              (end-at-last-punctuation  "In the light of the blue moon.  We danced merrily.  Be"))))))
